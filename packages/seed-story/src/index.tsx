ddbf6768620046125828fc2a91a64f4844f31338
Kevin Meinhardt <kmeinhardt7@gmail.com>
2022-10-10 05:55:19 +0000

    seed story pass faker instance

--- packages/seed-story/src/index.tsx
+++ packages/seed-story/src/index.tsx
@@ -1,23 +1,16 @@
 import { StoryObj } from "@storybook/react";
 import isChromatic from "chromatic/isChromatic";
-import { faker } from "@faker-js/faker";
-
-type FunctionOrObj<T> = T | ((seed: number) => T);
+import { faker, Faker } from "@faker-js/faker";
 
 // TODO: replace with environment variable or some configurable parameter
 const STORYBOOK_FAKER_SEED = 12345;
 
 export function seedStory<Args>(
-  getStoryObj: FunctionOrObj<StoryObj<Args>> = () => ({})
+  getStoryObj: (faker: Faker) => StoryObj<Args> = () => ({})
 ): StoryObj<Args> {
   if (isChromatic()) {
     faker.seed(STORYBOOK_FAKER_SEED);
   }
 
-  const story =
-    typeof getStoryObj === "function"
-      ? getStoryObj(STORYBOOK_FAKER_SEED)
-      : getStoryObj;
-
-  return story;
+  return getStoryObj(faker);
 }
